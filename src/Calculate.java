5c4c986c15590840f46e2a547e2048a3e0d1e7a7
willtriti03 <willtriti03@naver.com>
2018-09-20 11:16:10 +0900

    add,min,mul,div

--- src/Calculate.java
+++ src/Calculate.java
@@ -1,3 +1,4 @@
+﻿
 
 import java.util.Scanner;
 
@@ -10,10 +11,10 @@ public class Calculate {
 		int x, y;
 		String operator;
 		while (true) {
-			System.out.println("연산할 두 값을 입력하시오.");
+			System.out.println("�뿰�궛�븷 �몢 媛믪쓣 �엯�젰�븯�떆�삤.");
 			x = input.nextInt();
 			y = input.nextInt();
-			System.out.println("연산할 연산자를 입력하시오. (+, -, *, /)");
+			System.out.println("�뿰�궛�븷 �뿰�궛�옄瑜� �엯�젰�븯�떆�삤. (+, -, *, /)");
 			operator = input.next();
 			if (operator.equals("+")) {
 				System.out.println(x + " + " + y + " = " + add(x, y));
@@ -24,29 +25,31 @@ public class Calculate {
 			} else if (operator.equals("/")) {
 				System.out.println(x + " / " + y + " = " + div(x, y));
 			} else {
-				System.out.println("프로그램을 종료합니다.");
+				System.out.println("�봽濡쒓렇�옩�쓣 醫낅즺�빀�땲�떎.");
 				break;
 			}
 		}
 	}
 
 	public static int add(int x, int y) {
-		return 0;
+		return x+y;
 	}
 
 	public static int sub(int x, int y) {
 
-		return 0;
+		return x-y;
 	}
 
 	public static int mul(int x, int y) {
 
-		return 0;
+		return x*y;
 	}
 
 	public static double div(int x, int y) {
-		// 실수로 반환이 되도록 구현
-
-		return 0;
+		// �떎�닔濡� 諛섑솚�씠 �릺�룄濡� 援ы쁽
+		if(y!=0)
+			return x/y;
+		else
+			return 0;
 	}
 }
